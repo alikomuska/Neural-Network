93022771ea6736f7fd9db644a228a4c1928682f3
VasilisValeras <56166232+VasilisValeras@users.noreply.github.com>
2023-11-26 13:30:17 +0200

    Add files via upload

---
+++ Lab1/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define D 5
+#define K 4
+#define H1 5
+#define H2 5
+#define H3 5
+#define NUM_LAYERS 3
+#define FUNC 0 // 0 for logistic 1 for relu
+
+int neurons_per_layer[3] = {H1,H2,H3};
+
+struct Neuron {
+    double bias;
+    double *Weights;
+    double output;
+    int num_input;
+};
+
+struct Layer {
+    int num_neurons;
+    struct Neuron *neurons;
+};
+
+struct Neural_Network {
+    int num_layers;
+    struct Layer *layers;
+};
+
+void init_neurons(){
+
+}
+
+void init_layer(struct Layer* layer, int num_neurons){
+    layer->num_neurons = num_neurons;
+    layer->neurons = malloc(sizeof(struct Neuron) * num_neurons);
+
+    
+
+}
+
+void init_neural_network(struct Neural_Network* network, int num_layers){
+    network->num_layers = num_layers;
+    network->layers = malloc(sizeof(struct Layer) * num_layers);
+
+    for(int i = 0; i < network->num_layers; i++){
+        init_layer(&(network->layers[i]), neurons_per_layer[i]);
+    }
+
+}
+
+void forward_pass(float *x, int d, float *y, int k){
+    
+}
+
+int main(int argc, char* argv[]){
+
+    struct Neural_Network network;
+    //init_neural_network()
+}
